5a1091d5b8fb98ac7008ddaecfd4bdc6aaf8c956
Michael Gokey <michaelgokey@Michaels-MacBook-Pro.local>
2019-11-03 10:47:01 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          What I said. again and again no more again you bet baby
         </p>
         <a
           className="App-link"
